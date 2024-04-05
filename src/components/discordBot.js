9231a7341d81273301fa3955059fd5a1efd26ed1
Akhilesh Warty <akilesshw@gmail.com>
2024-04-04 20:30:49 -0400

    Made Discord Bot To Be A Component To Integrate With Future Dev
    Made discord bot to be a component to integrate with the future UI, functionality to be added later.

--- src/components/discordBot.js
+++ src/components/discordBot.js
@@ -9,280 +9,283 @@ import { Song } from './Song.js'
 
 configDotenv()
 
-const songQueue = new Queue()
+class DiscordBot{
+    constructor(){
+        this.songQueue = new Queue()
+        this.musicPlayer = createAudioPlayer()
+        this.recentlyPlayed = null
+        this.curerntlyPlaying = false
+        this.client = new Client({intents:[
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildVoiceStates,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent
+        ]})
+    }
 
-const musicPlayer = createAudioPlayer()
+    deployCommands = async (message)=>{
+        await message.guild.commands.set([
+            {
+                name:'play',
+                description:"Search for a song on youtube",
+                options:[
+                    {
+                        name:'query',
+                        description:'The song you want to search for',
+                        type:3,
+                        required:true
+                    }
+                ]
+            },
+            {
+                name:'ctrls',
+                description:"Music Controls"
+            },
+            {
+                name:'pause',
+                description:"Pause the current song"
+            },
+            {
+                name:'resume',
+                description:"Resume the current song"
+            },
+            {
+                name:'skip',
+                description:"Skip the current song"
+            }
+        ])
+    }
 
-let recentlyPlayed = null
+    parseMessage = async (message)=>{
+        console.log(message.content)
+    }
 
-let curerntlyPlaying = false
+    pausePlayback = async (interaction)=>{
+        this.musicPlayer.pause()
+        interaction.reply(`Playback paused by ${interaction.user.username}`)
+    }
 
-const client = new Client({intents:[
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-]})
+    resumePlayback = async (interaction)=>{
+        this.musicPlayer.unpause()
+        interaction.reply(`Playback resumed by ${interaction.user.username}`)
+    }
 
-const deployCommands = async (message)=>{
-    await message.guild.commands.set([
-        {
-            name:'play',
-            description:"Search for a song on youtube",
-            options:[
-                {
-                    name:'query',
-                    description:'The song you want to search for',
-                    type:3,
-                    required:true
-                }
-            ]
-        },
-        {
-            name:'ctrls',
-            description:"Music Controls"
-        },
-        {
-            name:'pause',
-            description:"Pause the current song"
-        },
-        {
-            name:'resume',
-            description:"Resume the current song"
-        },
-        {
-            name:'skip',
-            description:"Skip the current song"
+    skipSong = async (interaction)=>{
+        this.musicPlayer.stop()
+        interaction.reply(`Song skipped by ${interaction.user.username}`)
+    }
+    
+    musicSearch = async (query)=>{
+        const metadata = await play.search(query, { limit: 1 })
+    
+        const song = new Song(metadata[0].title,metadata[0].channel.name,metadata[0].durationInSec,metadata[0].url,metadata[0].thumbnails[0].url)
+    
+        return song
+    }
+
+    linkSearch = async (link)=>{
+        const metadata = await play.video_info(link)
+    
+        const song = new Song(metadata.video_details.title,metadata.video_details.channel.name,metadata.video_details.durationInSec,metadata.video_details.url,metadata.video_details.thumbnails[0].url)
+    
+        return song
+    }
+
+    cycleQueue = async (interaction)=>{
+        while(!this.songQueue.isEmpty()){
+    
+            const song = this.songQueue.peek()
+    
+            let stream = await play.stream(song.url)
+        
+            const voiceConnection = joinVoiceChannel({
+                channelId:interaction.member.voice.channel.id,
+                guildId:interaction.guildId,
+                adapterCreator:interaction.guild.voiceAdapterCreator
+            })
+        
+            let resource = createAudioResource(stream.stream, {
+                inputType: stream.type
+            })
+        
+            const playPromise = new Promise((resolve, reject) => {
+                this.musicPlayer.play(resource);
+                this.musicPlayer.on('idle', () => {
+                    resolve();
+                });
+                this.musicPlayer.on('error', (error) => {
+                    reject(error);
+                });
+            });
+        
+            // await musicPlayer.play(resource)
+        
+            voiceConnection.subscribe( this.musicPlayer)
+            await playPromise;
+            this.songQueue.dequeue()
+            this.recentlyPlayed = song
         }
-    ])
-}
-
-const parseMessage = async (message)=>{
-    console.log(message.content)
-}
-
-const pausePlayback = async (interaction)=>{
-    musicPlayer.pause()
-    interaction.reply(`Playback paused by ${interaction.user.username}`)
-}
-
-const resumePlayback = async (interaction)=>{
-    musicPlayer.unpause()
-    interaction.reply(`Playback resumed by ${interaction.user.username}`)
-}
-
-const skipSong = async (interaction)=>{
-    musicPlayer.stop()
-    interaction.reply(`Song skipped by ${interaction.user.username}`)
-}
-
-
-const musicSearch = async (query)=>{
-    const metadata = await play.search(query, { limit: 1 })
-
-    const song = new Song(metadata[0].title,metadata[0].channel.name,metadata[0].durationInSec,metadata[0].url,metadata[0].thumbnails[0].url)
-
-    return song
-}
-
-const linkSearch = async (link)=>{
-    const metadata = await play.video_info(link)
-
-    const song = new Song(metadata.video_details.title,metadata.video_details.channel.name,metadata.video_details.durationInSec,metadata.video_details.url,metadata.video_details.thumbnails[0].url)
-
-    return song
-}
-
-const cycleQueue = async (interaction)=>{
-    while(!songQueue.isEmpty()){
-
-        const song = songQueue.peek()
-
-        let stream = await play.stream(song.url)
+    }
     
-        const voiceConnection = joinVoiceChannel({
-            channelId:interaction.member.voice.channel.id,
-            guildId:interaction.guildId,
-            adapterCreator:interaction.guild.voiceAdapterCreator
+    playSongFromQueue = async (interaction)=>{
+        if(this.songQueue.isEmpty()) return
+    
+        this.curerntlyPlaying = true
+        
+        await  this.cycleQueue(interaction).then(()=>{
+            this.curerntlyPlaying = false
+        })
+       
+    }
+
+    addToQueue = async (song) => {
+
+        this.songQueue.enqueue(song);
+    
+        return song;
+    };
+
+    displayQueue = async (message) =>{
+        if( this.songQueue.isEmpty()) return message.reply('Queue is empty')
+    
+        const queue =  this.songQueue.getQueue()
+    
+        let queueMessage = 'Queue:\n'
+    
+        queue.forEach((song,index)=>{
+            queueMessage += `${index+1}. ${song.getMetadata()}\n`
         })
     
-        let resource = createAudioResource(stream.stream, {
-            inputType: stream.type
-        })
+        message.reply(queueMessage)
+    }
+
+    displayMusicControlButtons = async (interaction) =>{
+        const confirm = new ButtonBuilder()
+                .setCustomId('play')
+                .setLabel('Play')
+                .setStyle(ButtonStyle.Primary);
     
-        const playPromise = new Promise((resolve, reject) => {
-            musicPlayer.play(resource);
-            musicPlayer.on('idle', () => {
-                resolve();
-            });
-            musicPlayer.on('error', (error) => {
-                reject(error);
-            });
+            const pause = new ButtonBuilder()
+                .setCustomId('pause')
+                .setLabel('Pause')
+                .setStyle(ButtonStyle.Secondary);
+    
+            const skip = new ButtonBuilder()
+                .setCustomId('skip')
+                .setLabel('Skip')
+                .setStyle(ButtonStyle.Secondary);
+    
+            const row = new ActionRowBuilder()
+                .addComponents(confirm, pause, skip);
+    
+        let response = await interaction.reply({
+            content: 'Music Controls',
+            components:[row]})
+    
+        const filter = (i) => i.customId === 'play' || i.customId === 'pause' || i.customId === 'skip';
+    
+        const collector = response.createMessageComponentCollector({ filter, time: 15000 });
+    
+        collector.on('collect', async i => {
+            if(i.customId === 'play'){
+                await  this.resumePlayback(interaction)
+            }
+            if(i.customId === 'pause'){
+                await  this.pausePlayback(interaction)
+            }
+            if(i.customId === 'skip'){
+                await  this.skipSong(interaction)
+            }
         });
+    }
     
-        // await musicPlayer.play(resource)
-    
-        voiceConnection.subscribe(musicPlayer)
-        await playPromise;
-        songQueue.dequeue()
-        recentlyPlayed = song
-    }
-}
+    run = async ()=>{
+        this.client.once('ready',()=>{
+            console.log('Bot is ready')
+        })
 
-const playSongFromQueue = async (interaction)=>{
-    if(songQueue.isEmpty()) return
-
-    curerntlyPlaying = true
-    
-    await cycleQueue(interaction).then(()=>{
-        curerntlyPlaying = false
-    })
-   
-}
-
-const addToQueue = async (song) => {
-
-    songQueue.enqueue(song);
-
-    return song;
-};
-
-const displayQueue = async (message) =>{
-    if(songQueue.isEmpty()) return message.reply('Queue is empty')
-
-    const queue = songQueue.getQueue()
-
-    let queueMessage = 'Queue:\n'
-
-    queue.forEach((song,index)=>{
-        queueMessage += `${index+1}. ${song.getMetadata()}\n`
-    })
-
-    message.reply(queueMessage)
-}
-
-const displayMusicControlButtons = async (interaction) =>{
-    const confirm = new ButtonBuilder()
-			.setCustomId('play')
-			.setLabel('Play')
-			.setStyle(ButtonStyle.Primary);
-
-		const pause = new ButtonBuilder()
-			.setCustomId('pause')
-			.setLabel('Pause')
-			.setStyle(ButtonStyle.Secondary);
-
-        const skip = new ButtonBuilder()
-            .setCustomId('skip')
-            .setLabel('Skip')
-            .setStyle(ButtonStyle.Secondary);
-
-		const row = new ActionRowBuilder()
-			.addComponents(confirm, pause, skip);
-
-    let response = await interaction.reply({
-        content: 'Music Controls',
-        components:[row]})
-
-    const filter = (i) => i.customId === 'play' || i.customId === 'pause' || i.customId === 'skip';
-
-    const collector = response.createMessageComponentCollector({ filter, time: 15000 });
-
-    collector.on('collect', async i => {
-        if(i.customId === 'play'){
-            await resumePlayback(interaction)
-        }
-        if(i.customId === 'pause'){
-            await pausePlayback(interaction)
-        }
-        if(i.customId === 'skip'){
-            await skipSong(interaction)
-        }
-    });
-}
-
-client.once('ready',()=>{
-    console.log('Bot is ready')
-})
-
-client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-  
-    if (message.content === '!deploy') {
-      deployCommands(message)
-    }
-
-    if (message.content === '!queue') {
-        displayQueue(message)
-    }
-
-});
-
-client.on('interactionCreate', async (interaction)=>{
-    if(!interaction.isCommand()) return
-
-    if(!interaction.guildId) return
-
-    if (!(interaction.member instanceof GuildMember)) return
-
-    if (!interaction.member.voice.channel) {
-        return interaction.reply('You need to be in a voice channel')
-    }
-
-    const {commandName,options} = interaction
-
-    if(commandName === 'play'){
-        const query = options.getString('query')
-
-        if(query.includes('https://')){
-            let song = await linkSearch(query)
-            if(songQueue.size() === 0){
-                interaction.reply(`Playing ${song.title}`)
-                await addToQueue(song)
-                await playSongFromQueue(interaction)
-            }else{
-                await addToQueue(song)
-                interaction.reply(`Added ${song.title} to queue`)
+        this.client.on('messageCreate', async (message) => {
+            if (message.author.bot) return;
+          
+            if (message.content === '!deploy') {
+                this.deployCommands(message)
             }
-        }else{
-            let song = await musicSearch(query)
-
-            if(songQueue.size() === 0){
-                interaction.reply(`Playing ${song.title} by ${song.artist}`)
-                await addToQueue(song)
-                await playSongFromQueue(interaction)
-            }else{
-                await addToQueue(song)
-                interaction.reply(`Added ${song.title} by ${song.artist} to queue`)
+        
+            if (message.content === '!queue') {
+                this.displayQueue(message)
             }
-        }
-       
+        
+        });
 
-       
-    }
-    if(commandName === 'yl'){
+        this.client.on('interactionCreate', async (interaction)=>{
+            if(!interaction.isCommand()) return
+        
+            if(!interaction.guildId) return
+        
+            if (!(interaction.member instanceof GuildMember)) return
+        
+            if (!interaction.member.voice.channel) {
+                return interaction.reply('You need to be in a voice channel')
+            }
+        
+            const {commandName,options} = interaction
+        
+            if(commandName === 'play'){
+                const query = options.getString('query')
+        
+                if(query.includes('https://')){
+                    let song = await  this.linkSearch(query)
+                    if( this.songQueue.size() === 0){
+                        interaction.reply(`Playing ${song.title}`)
+                        await  this.addToQueue(song)
+                        await  this.playSongFromQueue(interaction)
+                    }else{
+                        await  this.addToQueue(song)
+                        interaction.reply(`Added ${song.title} to queue`)
+                    }
+                }else{
+                    let song = await  this.musicSearch(query)
+        
+                    if( this.songQueue.size() === 0){
+                        interaction.reply(`Playing ${song.title} by ${song.artist}`)
+                        await  this.addToQueue(song)
+                        await  this.playSongFromQueue(interaction)
+                    }else{
+                        await  this.addToQueue(song)
+                        interaction.reply(`Added ${song.title} by ${song.artist} to queue`)
+                    }
+                }
+               
+        
+               
+            }
+            if(commandName === 'yl'){
+                
+            }
+        
+            if(commandName === 'ctrls'){
+                this.displayMusicControlButtons(interaction)
+            }
+        
+            if(commandName === 'pause'){
+                this.pausePlayback(interaction)
+            }
+        
+            if(commandName === 'resume'){
+                this.resumePlayback(interaction)
+            }
+        
+            if(commandName === 'skip'){
+                this.skipSong(interaction)
+            }
+        })
+
+        this.client.login(process.env.DISCORD_TOKEN)
         
     }
+    
+}
 
-    if(commandName === 'ctrls'){
-        displayMusicControlButtons(interaction)
-    }
+let bot = new DiscordBot()
 
-    if(commandName === 'pause'){
-        pausePlayback(interaction)
-    }
-
-    if(commandName === 'resume'){
-        resumePlayback(interaction)
-    }
-
-    if(commandName === 'skip'){
-        skipSong(interaction)
-    }
-
-
-
-
-})
-
-client.login(process.env.DISCORD_TOKEN)
+bot.run()
