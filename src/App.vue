47d66f94d80a686ab899c4eda7f324ffece79b2b
Akhilesh Warty <akilesshw@gmail.com>
2024-04-28 23:26:38 -0400

    Created New Component
    Created new component successfully and added it to the main screen. Need to add some CSS and hook the discord bot to get music metadata

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
   import './assets/splash.css'
+  import QueueDisplay from './components/QueueDisplay.vue';
+  
 </script>
 
 <template>
-  <div class="bg-white h-screen w-svw">
-    <div>
-
+  <div style="background-color: #222222;">
+    <div class="pt-10 pl-10" style="background: transparent">
+      <h1 class="text-white text-5xl">Admin Dashboard</h1>
+      <div class="grid grid-cols-3 gap-4 h-svh">
+        <div class="bg-gray-600">Number Of Users Active</div>
+        <div>Element 2</div>
+        <div>Top Song</div>
+        <div class="row-start-2 row-end-4"><QueueDisplay/></div>
+        <div class="row-span-3">User Request Info</div>
+      </div>
     </div>
   </div>
 </template>
+
---
+++ src/components/QueueDisplay.vue
@@ -0,0 +1,18 @@
+<script>
+import { ref } from 'vue';
+
+const queue = ref([])
+</script>
+
+<template>
+  <div class="bg-gray-600">
+    <div>
+        <h2>Queue</h2>
+        <ul>
+            <li v-for="song in queue" :key="song.id">
+            {{ song.title }}
+            </li>
+        </ul>
+    </div>
+  </div>
+</template>
