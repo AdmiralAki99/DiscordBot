bdb4c31c04a7afc2691a4ade772afd1703b70851
Akhilesh Warty <akilesshw@gmail.com>
2024-05-08 21:15:07 -0400

    Added Vuestic UI Environment To The Applicaiton
    Added Vuestic UI env to the application to use all the UI elements properly

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
   import './assets/splash.css'
   import QueueDisplay from './components/QueueDisplay.vue';
   import CurrentMetadata from './components/CurrentMetadata.vue';
+  import ActiveUserDisplay from './components/ActiveUserDisplay.vue';
   export default {
     components: {
       QueueDisplay,
-      CurrentMetadata
+      CurrentMetadata,
+      ActiveUserDisplay
     }
   }
 </script>
 
 <template>
-  <div style="background-color: #222222;">
-    <div class="pt-10 pl-10" style="background: transparent">
-      <h1 class="text-white text-5xl">Admin Dashboard</h1>
+  <div>
+    <div class="pt-10 pl-10 pr-10" style="background: transparent">
+      <h1 class="text-white text-5xl pb-5">Admin Dashboard</h1>
       <div class="grid grid-cols-3 gap-4 h-svh">
-        <div class="bg-gray-600">Number Of Users Active</div>
+        <div><ActiveUserDisplay/></div>
         <div>Element 2</div>
         <div><CurrentMetadata/></div>
         <div class="row-start-2 row-end-4"><QueueDisplay/></div>
